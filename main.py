13edf408e71c1c06b3f56900cdbfae12b408c4e3
chunting-linct <chunting.linct@gmail.com>
2026-09-13 14:24:42 +0800

    Update: add driver two and refactor

--- main.py
+++ main.py
@@ -2,61 +2,70 @@ from gpiozero import DigitalOutputDevice
 
 from time import sleep
 
-ain1_1 = DigitalOutputDevice(26)
-ain2_1 = DigitalOutputDevice(21)
-stby_1 = DigitalOutputDevice(20)
-bin1_1 = DigitalOutputDevice(19)
-bin2_1 = DigitalOutputDevice(13)
-pwmb_1 = DigitalOutputDevice(12)
-pwma_1 = DigitalOutputDevice(5)
+driver_1 = {
+    "ain1" : DigitalOutputDevice(26),
+    "ain2" : DigitalOutputDevice(21),
+    "stby" : DigitalOutputDevice(20),
+    "bin1" : DigitalOutputDevice(19),
+    "bin2" : DigitalOutputDevice(13),
+    "pwmb" :DigitalOutputDevice(12),
+    "pwma" : DigitalOutputDevice(5)
+}
 
-def a_1_foward():
-    stby_1.on()
-    pwma_1.on()
-    ain1_1.on()
-    ain2_1.off()
-    sleep(2)
-    stby_1.off()
-    pwma_1.off()
-    ain1_1.off()
-    ain2_1.off()
-def a_1_backward():
-    stby_1.on()
-    pwma_1.on()
-    ain1_1.off()
-    ain2_1.on()
-    sleep(2)
-    stby_1.off()
-    pwma_1.off()
-    ain1_1.off()
-    ain2_1.off()
+driver_2 = {
+    "ain1" : DigitalOutputDevice(22),
+    "ain2" : DigitalOutputDevice(27),
+    "stby" : DigitalOutputDevice(17),
+    "bin1" : DigitalOutputDevice(18),
+    "bin2" : DigitalOutputDevice(23),
+    "pwmb" :DigitalOutputDevice(24),
+    "pwma" : DigitalOutputDevice(4)
+}
 
 
-def b_1_foward():
-    stby_1.on()
-    pwmb_1.on()
-    bin1_1.on()
-    bin2_1.off()
+def a_1_foward(driver):
+    driver["stby"].on()
+    driver["pwma"].on()
+    driver["ain1"].on()
+    driver["ain2"].off()
     sleep(2)
-    stby_1.off()
-    pwmb_1.off()
-    bin1_1.off()
-    bin2_1.off()
-def b_1_backward():
-    stby_1.on()
-    pwmb_1.on()
-    bin1_1.off()
-    bin2_1.on()
+    for pin in driver:
+        driver[pin].off()
+def a_1_backward(driver):
+    driver["stby"].on()
+    driver["pwma"].on()
+    driver["ain1"].off()
+    driver["ain2"].on()
     sleep(2)
-    stby_1.off()
-    pwmb_1.off()
-    bin1_1.off()
-    bin2_1.off()
+    for pin in driver:
+        driver[pin].off()
+
+def b_1_foward(driver):
+    driver["stby"].on()
+    driver["pwmb"].on()
+    driver["bin1"].on()
+    driver["bin2"].off()
+    sleep(2)
+    for pin in driver:
+        driver[pin].off()
+
+def b_1_backward(driver):
+    driver["stby"].on()
+    driver["pwmb"].on()
+    driver["bin1"].off()
+    driver["bin2"].on()
+    sleep(2)
+    for pin in driver:
+        driver[pin].off()
 
 
 
 if __name__ == '__main__':
-    a_1_foward()
-    a_1_backward()
-    b_1_foward()
-    b_1_backward()
+    a_1_foward(driver_1)
+    a_1_backward(driver_1)
+    b_1_foward(driver_1)
+    b_1_backward(driver_1)
+    a_1_foward(driver_2)
+    a_1_backward(driver_2)
+    b_1_foward(driver_2)
+    b_1_backward(driver_2)
